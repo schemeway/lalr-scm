fbdea45ad1853e5199923ff1de5b3f0966ec6263
Dominique Boucher <schemeway@gmail.com>
2010-03-19 13:09:54 +0000

    added version number

--- lalr.scm
+++ lalr.scm
@@ -16,6 +16,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+(define *lalr-scm-version* 2.4.1)
+
+
 (cond-expand 
 
  ;; -- Gambit-C
